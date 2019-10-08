0ad7ae832d44b92b6cc64f9362cf49e045210a6e
Darrian Soto <ddog350@gmail.com>
2019-10-08 16:12:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |item, value|
     if lowest_value == 0 || value < lowest_value
       lowest_value = value
-      empty = key
+      empty = item
     end
   end
   empty
